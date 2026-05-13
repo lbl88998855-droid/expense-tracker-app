f1e2dd631bc6ba1a7c5839232184a87aba2dc25d
lbl88998855-droid <lbl88998855@gmail.com>
2026-05-13 12:24:32 +0800

    Add build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+// Project-level build.gradle.kts
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
+}
+
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
